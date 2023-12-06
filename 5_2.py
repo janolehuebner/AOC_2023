f1e365d55f550d51638dfbb129d97eac16175d74
Jan-Ole Hübner <huebner@jan-ole.de>
2023-12-06 01:46:54 +0100

    create file for part 2

--- 5_2.py
+++ 5_2.py
@@ -1,22 +1,38 @@
 def get_blocks():
-    seeds, *blocks = open("5.in").read().split("\n\n")
-    seeds = list(map(int, seeds.split(":")[1].strip().split()))
+    seed_inputs, *blocks = open("5.in").read().split("\n\n")
+    seed_inputs = list(map(int, seed_inputs.split(":")[1].strip().split()))
+    seeds = []
+
+    for i in range(0, len(seed_inputs), 2):
+        seeds.append((seed_inputs[i], seed_inputs[i] + seed_inputs[i + 1]))
+
     return blocks, seeds
 
 blocks, seeds = get_blocks()
 
 for block in blocks:
     ranges = []
+    converted_seeds = []
     for line in block.splitlines()[1:]:
         ranges.append(list(map(int, line.split())))
-    converted_seeds = []
-    for x in seeds:
-        for dest, source, length in ranges: #is the value mapped?
-            if source <= x < source + length:
-                converted_seeds.append(x - source + dest) #new value
+
+    while len(seeds):
+        start,end = seeds.pop()
+        for dest, source, length in ranges:
+            overlap_start = max(start, source) #TODO: i don't really understand the overlapping yet .... friend did this
+            overlap_end = min(end, source+length)
+            if overlap_start < overlap_end:
+                converted_seeds.append((overlap_start - source +dest, overlap_end - source +dest))
+                if overlap_start > start:
+                    seeds.append((start,overlap_start))
+                if end > overlap_end:
+                    seeds.append((overlap_end, end))
                 break
-        else: # no= then it keeps its
-            converted_seeds.append(x)
+        else:
+            converted_seeds.append((start,end))
+
+
+
     seeds = converted_seeds
 
-print(min(seeds))
+print(min(seeds)[0])
